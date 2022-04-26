2e67112f001f00a9d1cc6036d6828e556a7c1db4
alexg <alexg@DESKTOP-STE2090>
2022-04-26 03:28:48 -0500

    Agregar archivos de proyecto.

---
+++ AlexisSigchaExamen/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AlexisSigchaExamen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+            if ((user == "Gabriel") && (pass == "Gabo123"))
+            {
+                await Navigation.PushAsync(new Registro(user));
+
+            }
+            else
+            {
+                await DisplayAlert("Alerta", "Credenciales Incorectas", "cerrar");
+            }
+        }
+    }
+}
